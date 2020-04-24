65619f1e07013eefb135a1b053b7135a96c2286b
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-04-24 10:51:50 -0700

    start to work on ui logic

--- src/main.js
+++ src/main.js
@@ -2,3 +2,9 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
+
+$("form#converter").submit(function(event){
+  event.preventDefault();
+  let amount = $("input#amount").val();
+  let currency = $("#currency").val();
+})
